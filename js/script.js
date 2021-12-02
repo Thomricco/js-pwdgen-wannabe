175bb88eea0d4a999a1dd5f9462a1b8d01e177be
Thomricco <tommasoricco@hotmail.it>
2021-12-02 17:08:40 +0100

    spero sia giusto

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 // chiediamo all'utente il suo nome
+const userName = prompt('Inserisci il tuo nome');
+
+
+
 
 // chiediamo all'utente il suo cognome
 
+const userLastname = prompt("Inserisci il tuo cognome")
+
+
+
+
+
 // chiediamo all'utente il suo colore preferito
+
+const colore = prompt("Inserisci il tuo colore preferito")
+
+
+
 
 
 // scrivo una variabile con nomecognomecolorepreferito21
+
+let htmlElement = document.getElementById('password');
+htmlElement.innerHTML = `la tua password è ${userName} ${userLastname} ${colore} 21`;
+
+console.log(`${userName} ${userLastname} ${colore} 21`);
