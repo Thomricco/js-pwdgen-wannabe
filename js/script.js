9fc11963c7a3f1943fb5dfe75e7a2325ecf995a7
Thomricco <tommasoricco@hotmail.it>
2021-12-02 16:39:59 +0100

    inizio js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// chiediamo all'utente il suo nome
+
+// chiediamo all'utente il suo cognome
+
+// chiediamo all'utente il suo colore preferito
+
+
+// scrivo una variabile con nomecognomecolorepreferito21
